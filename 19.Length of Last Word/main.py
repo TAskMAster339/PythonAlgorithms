cc4f643f1d2a44c375e2295ff6d69850b999d2f5
Dima <dimagr3214@gmail.com>
2024-12-09 22:29:02 +0300

    add Length of last word

---
+++ 19.Length of Last Word/main.py
@@ -0,0 +1,19 @@
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        count = 0
+        for i in range(len(s) -1, -1, -1):
+            if count == 0 and s[i] == " ":
+                continue
+            if s[i] != " ":
+                count += 1
+            else:
+                break
+        return count
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.lengthOfLastWord("Hello World"))
+    print(s.lengthOfLastWord("   fly me   to   the moon  "))
+    print(s.lengthOfLastWord("luffy is still joyboy"))
