5450eda91c79db886d88680e8baa7fcbeedf9d12
Dima <dimagr3214@gmail.com>
2024-12-09 22:25:52 +0300

    add Integer to Roman

---
+++ 18.Integer to Roman/main.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        table = {
+            1000: "M",
+            900: "CM",
+            500: "D",
+            400: "CD",
+            100: "C",
+            90: "XC",
+            50: "L",
+            40: "XL",
+            10: "X",
+            9: "IX",
+            5: "V",
+            4: "IV",
+            1: "I"
+        }
+        result = ""
+        for i in table.keys():
+            while num >= i:
+                num -= i
+                result += table[i]
+
+        return result
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.intToRoman(3749))
+    print(s.intToRoman(58))
+    print(s.intToRoman(1994))
