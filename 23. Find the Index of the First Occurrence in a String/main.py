78ca9df2e1b9edeab4e4429279ccad1f2e6a6982
Dima <dimagr3214@gmail.com>
2024-12-11 22:11:27 +0300

    add Find the Index of the First Occurrence in s String

---
+++ 23. Find the Index of the First Occurrence in a String/main.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.strStr("sadbutsad", "sad"))
+    print(s.strStr("leetcode", "leeto"))
