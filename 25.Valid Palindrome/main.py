d6028830a576ac89e70d1d2b68f6ea284b187975
Dima <dimagr3214@gmail.com>
2024-12-14 17:38:59 +0300

    add Valid Palindrome

---
+++ 25.Valid Palindrome/main.py
@@ -0,0 +1,16 @@
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        result = ""
+        for char in s:
+            if char.isalnum():
+                result += char
+        return result == result[::-1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isPalindrome("A man, a plan, a canal: Panama"))
+    print(s.isPalindrome("race a car"))
+    print(s.isPalindrome(" "))
