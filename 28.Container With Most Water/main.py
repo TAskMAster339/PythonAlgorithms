73632df76b66b4be457bbd465b5528549dbd2bf0
Dima <dimagr3214@gmail.com>
2024-12-15 13:50:00 +0300

    add Container With Most Water

---
+++ 28.Container With Most Water/main.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        maxArea = 0
+        left, right = 0, len(height) - 1
+        while left < right:
+            maxArea = max(maxArea, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxArea
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+    print(s.maxArea([1, 1]))
