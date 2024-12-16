0c097d7882e896ff3291ee05c23a6d154e0b24b0
Dima <dimagr3214@gmail.com>
2024-12-16 13:58:40 +0300

    add Minimum Size Subarray Sum

---
+++ 30.Minimum Size Subarray Sum/main.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        right = 0
+        cur_sum = nums[left]
+        min_len = 1_000_000
+
+        while right != len(nums) - 1 or left != len(nums):
+            if cur_sum >= target:
+                min_len = min(min_len, right - left + 1)
+                cur_sum -= nums[left]
+                left += 1
+                continue
+            else:
+                right += 1
+                if right >= len(nums):
+                    return min_len if min_len != 1_000_000 else 0
+            cur_sum += nums[right]
+        return min_len if min_len != 1_000_000 else 0
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.minSubArrayLen(7, [2, 3, 1, 2, 4, 3]))
+    print(s.minSubArrayLen(4, [1, 4, 4]))
+    print(s.minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1]))
+    print(s.minSubArrayLen(11, [1, 2, 3, 4, 5]))  # 3
+    print(s.minSubArrayLen(7, [1, 1, 1, 1, 7]))
