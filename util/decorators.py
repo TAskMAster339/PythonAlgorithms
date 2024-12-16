fd29f0422ba58992adcfd418b600324500750726
Dima <dimagr3214@gmail.com>
2024-12-17 00:15:19 +0300

    add util lib

---
+++ util/decorators.py
@@ -0,0 +1,12 @@
+import time
+import functools
+
+
+def timer(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter_ns()
+        result = f(*args, **kwargs)
+        print(f"Time: {(time.perf_counter_ns()-start) / (10**9):.7f}s")
+        return result
+    return wrapper
