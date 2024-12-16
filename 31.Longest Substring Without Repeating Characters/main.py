caa28b7b48f92d763cbc195fc4b3442c4c564ec7
Dima <dimagr3214@gmail.com>
2024-12-17 00:15:44 +0300

    add Longest Substing Without Repeating Characters

---
+++ 31.Longest Substring Without Repeating Characters/main.py
@@ -0,0 +1,28 @@
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) <= 1:
+            return len(s)
+
+        max_len = 0
+        start = 0
+        letter_set = {s[start]}
+
+        for end in range(1, len(s)):
+            while s[end] in letter_set:
+                letter_set.remove(s[start])
+                start += 1
+            else:
+                letter_set.add(s[end])
+                max_len = max(max_len, end - start + 1)
+
+        return max_len
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.lengthOfLongestSubstring("abcabcbb"))
+    print(s.lengthOfLongestSubstring("bbbbb"))
+    print(s.lengthOfLongestSubstring("pwwkew"))
+    print(s.lengthOfLongestSubstring(" "))
