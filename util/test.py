ac4c1551437180115070dcc4c0091f4408995ae8
Dima <dimagr3214@gmail.com>
2024-12-24 21:43:10 +0300

    add colorful output in test.py

--- util/test.py
+++ util/test.py
@@ -3,6 +3,15 @@ from typing import Callable, Any
 
 def test_case(function: Callable, result: Any, *args, **kwargs):
     call = function(*args, **kwargs)
-    print(f'Output is {call}')
-    assert call == result
-    print("OK")
+    if call == result:
+        print_green("Test case - OK")
+    else:
+        print_red(f'Test case - not OK\n\tOutput: {call}\n\tExpected: {result}')
+
+
+def print_green(text: str) -> None:
+    print("\033[3m\033[32m{}\033[0m".format(text))
+
+
+def print_red(text: str) -> None:
+    print("\033[3m\033[31m{}\033[0m".format(text))
