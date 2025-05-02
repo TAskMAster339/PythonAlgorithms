d5ead8aa5473a19896a05540a45a5b812aacf614
Dima <dimagr3214@gmail.com>
2025-05-02 13:59:34 +0300

    add Rotate List and txt

---
+++ 65.Rotate List/main.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        end = head
+        length = 1
+        if not end or not end.next:
+            return head
+
+        while end.next:
+            end = end.next
+            length += 1
+
+        k %= length
+
+        if k == 0:
+            return head
+
+        start = head
+        prev = None
+
+        for i in range(length - k):
+            prev = start
+            start = start.next
+
+        prev.next = None
+        end.next = head
+        return start
