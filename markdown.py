fea839299b6cfb8a570a27d1a9dadea9d0cfa15d
Dima <dimagr3214@gmail.com>
2025-05-23 22:34:10 +0300

    feat: add code to README.md

--- markdown.py
+++ markdown.py
@@ -5,6 +5,7 @@ import re
 from pathlib import Path
 
 file_lines = []
+code_lines = []
 
 HEADER_PATTERN = re.compile(r"\d+\) \w+")
 
@@ -14,6 +15,7 @@ ENGLISH_WORD_PATTERN = re.compile(r"[a-zA-Z0-9]+")
 
 BLOCK_WORDS = ("Условие:", "Идея:", "Реализация:", "Оценка:")
 
+CODE_FILE_NAME = "main.py"
 FILE_INPUT_NAME = "text.txt"
 FILE_OUTPUT_NAME = "tg.md"
 
@@ -50,13 +52,27 @@ def format_file(file_lines: list[str]) -> None:
             file_lines[i] = bold_english_words(line)
 
 
-def format_file_for_git(file_lines: list[str]) -> None:
+def format_file_for_git(file_lines: list[str], code_lines: list[str]) -> None:
     for i in range(len(file_lines)):
         file_lines[i] = file_lines[i].strip()
         if file_lines[i].strip("*") in BLOCK_WORDS:
             file_lines[i] = f"## {file_lines[i]}"
     link = file_lines.pop(1)
     file_lines[0] = f"# [{file_lines[0]}]({link})"
+
+    # add code
+    try:
+        with Path.open(
+            dir_path / CODE_FILE_NAME,
+            "r",
+            encoding="UTF-8",
+        ) as file:
+            code_lines.append("## Код:\n```python")
+            for line in file.readlines():
+                code_lines += [line.removesuffix("\n")]
+            code_lines.append("\n```\n")
+    except FileNotFoundError:
+        print("e")
 
 
 def get_path_from_regex(path_regex: str) -> Path:
@@ -102,15 +118,17 @@ if __name__ == "__main__":
 
         print(f"Created {FILE_OUTPUT_NAME} in {dir_path}")
 
-        format_file_for_git(file_lines)
+        format_file_for_git(file_lines, code_lines)
 
         with Path.open(
             dir_path / "README.md",
             "w",
             encoding="UTF-8",
         ) as file:
-            lines = [line + "\n\n" for line in file_lines]
-            file.writelines(lines)
+            file.writelines([line + "\n\n" for line in file_lines])
+
+            file.writelines([line + "\n" for line in code_lines])
+            print(code_lines)
 
         print(f"Created README.md in {dir_path}")
 
